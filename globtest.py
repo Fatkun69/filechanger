5d033c4b14bd97ab3af4b01b99125c60fa8c9e3b
KenHuang <ken103023@gmail.com>
2024-02-26 23:28:16 +0800

    test python os module

--- globtest.py
+++ globtest.py
@@ -2,8 +2,19 @@ import os
 import glob
 
 folder_name = []
-print(glob.glob(os.path.join("*")))
-folder_name.append(glob.glob(os.path.join("*")))
-print("folder name : " + str(folder_name))
+#print(glob.glob(os.path.join("*")))
 
-print(glob.glob(os.path.join("Folder1", "*")))
+#show file address now
+print(os.getcwd())
+
+
+'''get list data and store in list variable'''
+folder_name = glob.glob(os.path.join("*"))
+
+#folder_name.append(glob.glob(os.path.join("*")))
+#print("folder name : " + str(folder_name))
+
+#print(glob.glob(os.path.join("Folder1", "*")))
+
+print("list folder name : " + str(folder_name))
+
