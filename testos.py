2777c6e6607dce09f20577d5e99dd401bf3e0e74
KenHuang <ken103023@gmail.com>
2024-02-27 00:51:41 +0800

    test os object

--- testos.py
+++ testos.py
@@ -1,6 +1,22 @@
 import os
 '''test os object'''
 
-'''get file's address'''
+'''get file's address
 fileaddr = os.getcwd()
 print("file browser at : " + fileaddr)
+'''
+print("file browser at : " + os.getcwd())
+
+'''change work space'''
+'''os.chdir("D:")'''
+
+'''make dir'''
+'''os.mkdir("demo")'''
+'''os.mkdir("demo/hello.txt")under demo dir's hello.txt dir'''
+
+'''delete dir'''
+'''os.rmdir("demo")'''
+
+'''list all file in dir with list'''
+print(os.listdir("demo"))
+
