70bc6e036b48f1b7ac5798021d2757953eb8f2ab
KenHuang <ken103023@gmail.com>
2024-02-29 18:23:28 +0800

    write at list working space file and check exists or not

--- main.py
+++ main.py
@@ -8,32 +8,68 @@ import time
 reference data : https://www.delftstack.com/zh-tw/howto/python/how-to-find-files-with-certain-extension-only-in-python/
 '''
 
-'''get now address'''
-now_program_path = os.getcwd()
-'''show path'''
-print("work at : " + now_program_path)
-print("show file in folder")
-print(os.listdir(now_program_path))
-
-'''set target path or file'''
-'''print("select your target folder or file format")'''
-file_target = input("select your target folder or file format : ")
-print("select target : " + file_target)
-
-'''check file exist'''
-
-
 '''program exit with input 'exit' command'''
-while (1):
+while 1:
     user_command = input("want to exit program , input 'exit' command :")
-    if(user_command == 'exit'):
+    if user_command == 'exit':
         print("exit Program")
         break
     else:
         print("program Run")
 
-    '''end command if else'''
+        '''get now address'''
+        now_program_path = os.getcwd()
+
+        '''show path'''
+        print("work at : " + now_program_path)
+        print("show file in folder under")
+        '''print(os.listdir(now_program_path))'''
+        list_folder_data = os.listdir(now_program_path)
+        '''show file in vector line'''
+        for i in list_folder_data:
+            print(i)
+        '''end for i'''
+
+        '''set target path or file'''
+        '''print("select your target folder or file format")'''
+        file_target = input("select your target folder or file format : ")
+        print("select target : " + file_target)
+
+        '''check file exist'''
+        '''flag use bool variable'''
+        file_exists_flag = False
+        '''default is no'''
+        '''search file loop'''
+        while 1:
+            if os.path.exists(file_target):
+                '''true exit'''
+                file_exists_flag = True
+                print("file already exists")
+                break
+            else:
+                print("don't have target file")
+                user_command = input("don't want to select file or target , input 'exit' command to exit : ")
+                if user_command == 'exit':
+                    print("do not select file and exit")
+                    break
+                else:
+                    '''search file again'''
+                    file_target = user_command
+            '''end if else'''
+
+        '''end while 1'''
+
+    '''check flag status'''
+    if file_exists_flag:
+        print('file exists')
+    else:
+        print('do not have file')
+
+    '''end if else'''
 
 '''end while'''
 
-
+'''show message'''
+print("Before exit program show message under")
+print("This program is written by Handsome ken")
+print("米奇米奇操你媽")
